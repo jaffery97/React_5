6bfd994eab72ef559e21823f6f6ca7d099fc0bb7
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-03-10 19:46:27 -0400

    second update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Ticker extends Component {
 
-export default App;
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+  //LifeCycle methods
+  componentDidMount() {
+
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //this is where we check it's true or false using while
+    while(nextState.count % 3 === 0) return true;
+  }
+
+  reset = () => {
+    this.setState({
+      count: this.state.count = 0
+    })
+  }
+
+  //this is a custom method
+  render() {
+    return ( < div className = "main" > < p className = "ticker" > This ticker number is: {
+        this.state.count
+      } < /p>
+
+
+
+      <
+      button type = "button"
+      onClick = {
+        this.reset
+      }
+      className = "reset" > Reset < /button>
+
+
+      <
+      /div>
+
+
+
+    );
+
+  };
+};
+
+export default Ticker;
